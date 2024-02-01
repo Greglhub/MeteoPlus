0fbf297a5c78d4edebba50ca2e8579ce551a0455
Greglhub <76042882+Greglhub@users.noreply.github.com>
2024-02-01 09:37:27 +0100

    Puch project

--- screens/AddedToList.js
+++ screens/AddedToList.js
@@ -2,22 +2,22 @@ import * as React from "react";
 import { Text, StyleSheet, View } from "react-native";
 import { Image } from "expo-image";
 import NiceCardContainer from "../components/NiceCardContainer";
-import { Color, FontSize, FontFamily, Border } from "../GlobalStyles";
+import { FontSize, Color, FontFamily, Border } from "../GlobalStyles";
 
-const AddToList = () => {
+const AddedToList = () => {
   return (
-    <View style={styles.addToList}>
+    <View style={styles.addedToList}>
       <Text style={styles.paris}>Paris</Text>
-      <View style={[styles.addToListChild, styles.addBg]} />
+      <View style={styles.addedToListChild} />
       <Image
         style={[styles.groupIcon, styles.iconLayout]}
         contentFit="cover"
-        source={require("../assets/group2.png")}
+        source={require("../assets/group1.png")}
       />
       <Image
         style={[styles.vectorIcon, styles.iconLayout]}
         contentFit="cover"
-        source={require("../assets/vector3.png")}
+        source={require("../assets/vector8.png")}
       />
       <NiceCardContainer quantity="20" propTop={335} />
       <Text style={[styles.time, styles.fTypo]}>TIME</Text>
@@ -28,27 +28,23 @@ const AddToList = () => {
       <Text style={[styles.uv, styles.fTypo]}>UV</Text>
       <Text style={[styles.rain, styles.fTypo]}>% RAIN</Text>
       <Text style={[styles.f, styles.fPosition]}>F°</Text>
-      <View style={[styles.addToListItem, styles.addBg]} />
-      <Text style={[styles.addToList1, styles.fTypo]}>Add to list</Text>
+      <View style={styles.addedToListItem} />
       <Image
-        style={styles.iconcontentaddCircleOutlin}
+        style={[styles.iconactiondone24px, styles.iconLayout]}
         contentFit="cover"
-        source={require("../assets/iconcontentadd-circle-outline-24px.png")}
+        source={require("../assets/iconactiondone-24px.png")}
       />
+      <Text style={[styles.addedToList1, styles.fTypo]}>Added to list</Text>
       <Image
         style={[styles.iconnavigationarrowBackIos, styles.iconLayout]}
         contentFit="cover"
-        source={require("../assets/iconnavigationarrow-back-ios-24px1.png")}
+        source={require("../assets/iconnavigationarrow-back-ios-24px.png")}
       />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  addBg: {
-    backgroundColor: Color.colorGray_100,
-    position: "absolute",
-  },
   iconLayout: {
     maxHeight: "100%",
     maxWidth: "100%",
@@ -77,36 +73,38 @@ const styles = StyleSheet.create({
     fontSize: FontSize.size_11xl,
     fontWeight: "600",
     fontFamily: FontFamily.poppinsSemiBold,
+    color: Color.colorGray_200,
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
     width: 77,
     height: 30,
     textAlign: "center",
-    color: Color.colorGray_200,
     position: "absolute",
   },
-  addToListChild: {
+  addedToListChild: {
     top: 440,
     left: 24,
     borderRadius: Border.br_2xs,
+    backgroundColor: Color.colorGray_100,
     width: 327,
     height: 59,
+    position: "absolute",
   },
   groupIcon: {
     height: "15.15%",
     width: "32.8%",
-    top: "33%",
+    top: "33.01%",
     right: "33.6%",
-    bottom: "51.85%",
+    bottom: "51.84%",
     left: "33.6%",
   },
   vectorIcon: {
     height: "2.59%",
     width: "5.6%",
-    top: "36.33%",
+    top: "36.32%",
     right: "31.2%",
-    bottom: "61.08%",
+    bottom: "61.09%",
     left: "63.2%",
   },
   time: {
@@ -169,42 +167,48 @@ const styles = StyleSheet.create({
     color: Color.colorSilver,
     top: 446,
   },
-  addToListItem: {
+  addedToListItem: {
     top: 65,
     left: 241,
     borderRadius: Border.br_6xs,
+    backgroundColor: "#009456",
     width: 110,
     height: 39,
+    position: "absolute",
   },
-  addToList1: {
-    top: 77,
-    left: 255,
+  iconactiondone24px: {
+    height: "0.99%",
+    width: "2.93%",
+    top: "9.98%",
+    right: "8.53%",
+    bottom: "89.03%",
+    left: "88.53%",
+  },
+  addedToList1: {
+    top: 76,
+    left: 249,
+    color: "#fff",
     fontFamily: FontFamily.poppinsMedium,
     fontWeight: "500",
     position: "absolute",
     textAlign: "left",
     fontSize: FontSize.size_xs,
-    color: Color.colorGray_200,
-  },
-  iconcontentaddCircleOutlin: {
-    width: 14,
-    height: 14,
   },
   iconnavigationarrowBackIos: {
     height: "2.44%",
     width: "3.12%",
-    top: "9.24%",
+    top: "9.23%",
     right: "90.48%",
     bottom: "88.33%",
     left: "6.4%",
   },
-  addToList: {
+  addedToList: {
     backgroundColor: Color.colorTurquoise,
     flex: 1,
     width: "100%",
-    height: 812,
+    height: 808,
     overflow: "hidden",
   },
 });
 
-export default AddToList;
+export default AddedToList;
--- screens/Sunny.js
+++ screens/Sunny.js
@@ -2,125 +2,161 @@ import * as React from "react";
 import { Text, StyleSheet, View } from "react-native";
 import { Image } from "expo-image";
 import NiceCardContainer from "../components/NiceCardContainer";
+import Component4Icon from "../components/Component4Icon";
 import { Color, FontSize, FontFamily, Border } from "../GlobalStyles";
 
-const AddToList = () => {
+const Sunny = () => {
   return (
-    <View style={styles.addToList}>
-      <Text style={styles.paris}>Paris</Text>
-      <View style={[styles.addToListChild, styles.addBg]} />
+    <View style={styles.sunny}>
+      <Text style={styles.nice}>Nice</Text>
+      <View style={[styles.sunnyChild, styles.sunnyPosition1]} />
+      <View style={[styles.sunnyItem, styles.sunnyPosition]} />
+      <Image
+        style={styles.iconactionsearch24px}
+        contentFit="cover"
+        source={require("../assets/iconactionsearch-24px1.png")}
+      />
       <Image
         style={[styles.groupIcon, styles.iconLayout]}
         contentFit="cover"
-        source={require("../assets/group2.png")}
+        source={require("../assets/group3.png")}
       />
       <Image
         style={[styles.vectorIcon, styles.iconLayout]}
         contentFit="cover"
-        source={require("../assets/vector3.png")}
+        source={require("../assets/vector9.png")}
       />
-      <NiceCardContainer quantity="20" propTop={335} />
+      <NiceCardContainer quantity="25" propTop={360} />
       <Text style={[styles.time, styles.fTypo]}>TIME</Text>
+      <Text style={styles.searchLocation}>Search Location</Text>
       <Text style={[styles.am, styles.textTypo]}>11:25 AM</Text>
       <Text style={[styles.text, styles.textTypo]}>4</Text>
       <Text style={[styles.text1, styles.textTypo]}>58%</Text>
       <Text style={[styles.text2, styles.fPosition]}>22</Text>
       <Text style={[styles.uv, styles.fTypo]}>UV</Text>
       <Text style={[styles.rain, styles.fTypo]}>% RAIN</Text>
-      <Text style={[styles.f, styles.fPosition]}>F°</Text>
-      <View style={[styles.addToListItem, styles.addBg]} />
-      <Text style={[styles.addToList1, styles.fTypo]}>Add to list</Text>
-      <Image
-        style={styles.iconcontentaddCircleOutlin}
-        contentFit="cover"
-        source={require("../assets/iconcontentadd-circle-outline-24px.png")}
-      />
-      <Image
-        style={[styles.iconnavigationarrowBackIos, styles.iconLayout]}
-        contentFit="cover"
-        source={require("../assets/iconnavigationarrow-back-ios-24px1.png")}
-      />
+      <Text style={[styles.f, styles.fTypo]}>F°</Text>
+      <Component4Icon dimensionCode={require("../assets/component-41.png")} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  addBg: {
+  sunnyPosition: {
+    width: 360,
     backgroundColor: Color.colorGray_100,
+    left: 24,
     position: "absolute",
+  },
+  sunnyPosition1: {
+    width: 360,
+    backgroundColor: Color.colorGray_100,
+    left: 24,
+    position: "absolute",
+    top:600,
   },
   iconLayout: {
     maxHeight: "100%",
     maxWidth: "100%",
     position: "absolute",
     overflow: "hidden",
+    
   },
   fTypo: {
     textAlign: "left",
+    color: Color.colorSilver,
     fontSize: FontSize.size_xs,
+    top: 605,
   },
   textTypo: {
     color: Color.colorDarkgray,
+    top: 491,
     fontSize: FontSize.size_mini,
-    top: 466,
     textAlign: "center",
+    top: 625,
+  },
+  ttextTypo: {
+    color: Color.colorDarkgray,
+    top: 491,
+    fontSize: FontSize.size_mini,
+    textAlign: "60",
   },
   fPosition: {
     left: 313,
     fontFamily: FontFamily.poppinsMedium,
     fontWeight: "500",
     position: "absolute",
+    top: 625,
   },
-  paris: {
-    top: 289,
-    left: 149,
+  nice: {
+    top: 400,
+    left: 180,
     fontSize: FontSize.size_11xl,
-    fontWeight: "600",
+    fontWeight: "500",
     fontFamily: FontFamily.poppinsSemiBold,
+    color: Color.colorGray_200,
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
     width: 77,
-    height: 30,
+    height: 100,
     textAlign: "center",
-    color: Color.colorGray_200,
     position: "absolute",
+
+    
   },
-  addToListChild: {
-    top: 440,
-    left: 24,
+  sunnyChild: {
+    top: 465,
     borderRadius: Border.br_2xs,
-    width: 327,
     height: 59,
+    width: 150
+  },
+  sunnyItem: {
+    top: 80,
+    borderRadius: Border.br_mini,
+    height: 46,
+    
+  },
+  iconactionsearch24px: {
+    width: 17,
+    height: 17,
   },
   groupIcon: {
     height: "15.15%",
     width: "32.8%",
-    top: "33%",
+    top: "25%",
     right: "33.6%",
-    bottom: "51.85%",
+    bottom: "48.76%",
     left: "33.6%",
   },
   vectorIcon: {
     height: "2.59%",
     width: "5.6%",
-    top: "36.33%",
+    top: "43%",
     right: "31.2%",
-    bottom: "61.08%",
-    left: "63.2%",
+    bottom: "58.01%",
+    left: "60.2%",
   },
   time: {
     left: 60,
-    color: Color.colorSilver,
-    top: 446,
-    textAlign: "left",
-    fontSize: FontSize.size_xs,
+    
     fontFamily: FontFamily.poppinsMedium,
     fontWeight: "500",
     position: "absolute",
   },
+  searchLocation: {
+    top: 92,
+    fontFamily: FontFamily.poppinsRegular,
+    fontSize: FontSize.size_mini,
+    left: 44,
+    textAlign: "left",
+    color: Color.colorSilver,
+    position: "absolute",
+  },
   am: {
     left: 44,
+    color: Color.colorDarkgray,
+    top: 491,
     fontFamily: FontFamily.poppinsMedium,
     fontWeight: "500",
     position: "absolute",
@@ -139,72 +175,35 @@ const styles = StyleSheet.create({
   },
   text2: {
     color: Color.colorDarkgray,
+    top: 491,
     fontSize: FontSize.size_mini,
-    top: 466,
     textAlign: "center",
   },
   uv: {
     left: 155,
-    color: Color.colorSilver,
-    top: 446,
-    textAlign: "left",
-    fontSize: FontSize.size_xs,
     fontFamily: FontFamily.poppinsMedium,
     fontWeight: "500",
     position: "absolute",
   },
   rain: {
     left: 222,
-    color: Color.colorSilver,
-    top: 446,
-    textAlign: "left",
-    fontSize: FontSize.size_xs,
     fontFamily: FontFamily.poppinsMedium,
     fontWeight: "500",
     position: "absolute",
   },
   f: {
-    textAlign: "left",
-    fontSize: FontSize.size_xs,
-    color: Color.colorSilver,
-    top: 446,
-  },
-  addToListItem: {
-    top: 65,
-    left: 241,
-    borderRadius: Border.br_6xs,
-    width: 110,
-    height: 39,
-  },
-  addToList1: {
-    top: 77,
-    left: 255,
+    left: 320,
     fontFamily: FontFamily.poppinsMedium,
     fontWeight: "500",
     position: "absolute",
-    textAlign: "left",
-    fontSize: FontSize.size_xs,
-    color: Color.colorGray_200,
   },
-  iconcontentaddCircleOutlin: {
-    width: 14,
-    height: 14,
-  },
-  iconnavigationarrowBackIos: {
-    height: "2.44%",
-    width: "3.12%",
-    top: "9.24%",
-    right: "90.48%",
-    bottom: "88.33%",
-    left: "6.4%",
-  },
-  addToList: {
+  sunny: {
     backgroundColor: Color.colorTurquoise,
     flex: 1,
     width: "100%",
-    height: 812,
+    height: 808,
     overflow: "hidden",
   },
 });
 
-export default AddToList;
+export default Sunny;
