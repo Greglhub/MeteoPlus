405a5f478351c4d8120b169b9989122a8c3b81f3
BAMBA-FALL <khadimmbackefall1@gmail.com>
2024-02-04 19:36:01 +0100

    favoritesadd et favoritesadded

--- src/screens/FaddStyle.js
+++ src/screens/FaddStyle.js
@@ -1,48 +1,59 @@
+// FaddStyle.js
+
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#00d4ff',
-    alignItems: 'center', // Center content horizontally
-    justifyContent: 'center', // Center content vertically
+    padding: 20,
   },
   favoris: {
-    borderBottomColor: '#df8e00',
-    borderBottomWidth: 3,
-    fontSize: 19,
-    padding: 5,
-    marginVertical: 20,
-    marginHorizontal: 10,
-    width: '80%',
+    height: 40,
     borderColor: 'gray',
     borderWidth: 1,
-    padding: 10,
     marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+  favoriscard: {
+    marginTop: 20,
+  },
+  favoriscardItem: {
+    flexDirection: 'row',
     backgroundColor: '#fff',
     borderRadius: 10,
+    padding: 16,
+    marginVertical: 10,
+    marginHorizontal: 16,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 10,
+    elevation: 5,
   },
-  addToFavoritesButton: {
-    backgroundColor: 'green',
-    marginTop: 10,
-    padding: 10,
-    borderRadius: 5,
-    width: '40%', 
+  favorisText: {
+    fontSize: 16,
+    marginRight: 10,
   },
-  SuppToFavoritesButton: {
-    backgroundColor: 'red',
-    marginTop: 10,
-    padding: 10,
-    borderRadius: 5,
-    width: '100%', // Set the width to 80%
-  },
-  favorisTitle: {
-    fontSize: 18,
-    fontWeight: 'bold',
+  favorisButton: {
+    fontSize: 10,
     color: 'red',
-    marginTop: 10,
   },
-  
+
+  trashButton: {
+    backgroundColor: '#fff', // Customize background color if needed
+    borderColor: '#000', // Customize border color if needed
+    borderWidth: 1,
+    borderRadius: 50, // Make the button circular
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    flexDirection: 'row', // Align icon and text horizontally
+    alignItems: 'center', // Align icon and text vertically
+  },
+  trashIcon: {
+    marginRight: 5, // Adjust spacing between icon and text
+    fontSize: 18, // Customize icon size
+    color: '#000', // Customize icon color if needed
+  },
 });
 
 export default styles;
--- src/screens/FaddedStyle.js
+++ src/screens/FaddedStyle.js
@@ -1,36 +1,21 @@
 import { StyleSheet } from "react-native";
-import { Dimensions } from 'react-native';
 
-const { width, height } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
 
-    container: {
-        flex: 1,
-        backgroundColor: '#00d4ff',
-      },
-      weatherIcon: {
-        width: 50, 
-        height: 50, 
-        alignItems: 'center'
-      },
-      searchInput: {
-        borderBottomColor: '#df8e00',
-        borderBottomWidth: 3, 
-        fontSize: 19,
-        padding: 5,
-        paddingVertical: 20, 
-        marginVertical: 100,
-        marginHorizontal: 10,
-        height: "27%",
-        width: '80%',
-        borderColor: 'gray',
-        borderWidth: 1,
-        padding: 10,
-        marginBottom: 10,
-        backgroundColor: '#fff',  
+    favoriscard: {
+        flexDirection: 'row',
+        backgroundColor: '#fff',
         borderRadius: 10,
+        padding: 16,
+        margin: 16,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
       },
-    })
+})
 
-    export default styles
+
+export default styles
