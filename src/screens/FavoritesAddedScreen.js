7b73a88ff2c5a92945f89b612b1cbc97adcb445f
samaali mohamed amine <samalimouhamedamine@gmail.com>
2024-02-04 21:41:30 +0100

    addedF est mise a jours

--- src/screens/FavoritesAddedScreen.js
+++ src/screens/FavoritesAddedScreen.js
@@ -1,54 +1,49 @@
+// FavoritesListScreen.js
 import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList, AsyncStorage } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation } from '@react-navigation/native';
+import styles from './FaddedStyle'; // Assurez-vous d'importer les styles appropriés
 
-const FavoritesAddedScreen = ({ favorites }) => {
-  const [addedCities, setAddedCities] = useState([]);
+const FavoritesAddedScreen = () => {
+  const [favoriteCities, setFavoriteCities] = useState([]);
+  const navigation = useNavigation();
+
+  const loadFavoriteCities = async () => {
+    try {
+      const savedFavoriteCitiesString = await AsyncStorage.getItem('favorites');
+      if (savedFavoriteCitiesString) {
+        const savedFavoriteCities = JSON.parse(savedFavoriteCitiesString);
+        setFavoriteCities(savedFavoriteCities);
+      }
+    } catch (error) {
+      console.error('Error loading favorite cities:', error);
+    }
+  };
 
   useEffect(() => {
-    // Charger les villes ajoutées depuis AsyncStorage lors du montage initial du composant
-    const loadAddedCities = async () => {
-      try {
-        const savedAddedCities = await AsyncStorage.getItem('addedCities');
-        if (savedAddedCities) {
-          setAddedCities(JSON.parse(savedAddedCities));
-        }
-      } catch (error) {
-        console.error('Error loading added cities:', error);
-      }
-    };
+    const unsubscribe = navigation.addListener('focus', () => {
+      // Chargement des villes favorites à chaque fois que la page reçoit le focus
+      loadFavoriteCities();
+    });
 
-    loadAddedCities();
-  }, []);
-
-  // Mettre à jour les favoris dans AsyncStorage lorsque la prop favorites change
-  useEffect(() => {
-    const saveAddedCities = async () => {
-      try {
-        await AsyncStorage.setItem('addedCities', JSON.stringify(favorites));
-        setAddedCities(favorites);
-      } catch (error) {
-        console.error('Error saving added cities:', error);
-      }
-    };
-
-    saveAddedCities();
-  }, [favorites]);
+    // Nettoyage de l'écouteur lorsque le composant est démonté
+    return unsubscribe;
+  }, [navigation]);
 
   return (
-    <View>
-      <Text>Villes ajoutées :</Text>
-      {addedCities.length > 0 && (
-        <FlatList
-          data={addedCities}
-          keyExtractor={(item) => item}
-          renderItem={({ item }) => (
-            <View>
-              <Text>{item}</Text>
-              {/* Ajoutez ici tout autre rendu ou action nécessaire pour chaque ville ajoutée */}
-            </View>
-          )}
-        />
-      )}
+    <View style={styles.container}>
+      <Text style={styles.heading}> Favorites</Text>
+      <FlatList
+        data={favoriteCities}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <View style={styles.favoriteCityItem}>
+            <Text style={styles.favoriteCityText}>{item}</Text>
+            {/* Ajoutez ici d'autres éléments que vous souhaitez afficher pour chaque ville favorite */}
+          </View>
+        )}
+      />
     </View>
   );
 };
