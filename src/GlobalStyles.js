2239c11f0657fb0114069a3871247c1bcd763e44
samaali mohamed amine <samalimouhamedamine@gmail.com>
2024-02-04 23:21:33 +0100

    update style addedF et addF

---
+++ src/GlobalStyles.js
@@ -0,0 +1,22 @@
+// GlobalStyles.js
+export const Color = {
+    colorGray_100: '#CCCCCC',
+    colorGray_200: '#999999',
+    // Add more color definitions as needed
+  };
+  
+  export const FontSize = {
+    size_xs: 12,
+    // Add more font size definitions as needed
+  };
+  
+  export const FontFamily = {
+    poppinsMedium: 'Poppins-Medium',
+    // Add more font family definitions as needed
+  };
+  
+  export const Border = {
+    br_6xs: 4,
+    // Add more border definitions as needed
+  };
+  
